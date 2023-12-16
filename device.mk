0489364b170f40b020bf59ad91f6d3ca1cd656e3
Han Sol Jin <hansol@hansol.ca>
2023-12-15 18:22:37 -0800

    caymanslm: move hals.conf from vendor to stock
    G910EMW30l
    caymanslm-user 12 SKQ1.211103.001 231250317a5f6 release-keys
    
    Change-Id: I5cb72e5123ca494fa21de1fe8ea46b45469afda6

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
